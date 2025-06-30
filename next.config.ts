7122d376030a16536a9abeeb853fe6859564f156
Vijay Chavre <chavre.vijay@gmail.com>
2025-06-30 15:49:18 +0530

    Refactor next.config.ts and package.json for streamlined build process
    Removed unnecessary configurations from next.config.ts and simplified the build script in package.json. Commented out the contact-buttons CSS for potential future use, enhancing overall code clarity and maintainability.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-vijay-chavare' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-vijay-chavare/' : '',
+  }
 };
 
 export default nextConfig;
